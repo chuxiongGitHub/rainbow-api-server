88b8d35374e676c8254a3b7b62ee82db04762b81
chencaihong <flq900523>
2017-06-27 22:21:23 +0800

    加入beetle配置

---
+++ src/main/kotlin/com/rainbow/controller/api/ViewController.kt
@@ -0,0 +1,16 @@
+package com.rainbow.controller.api
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.servlet.ModelAndView
+
+/**
+ * Created by Administrator on 2017/6/27.
+ */
+@Controller
+class ViewController {
+
+
+    @GetMapping("/")
+    fun default()=ModelAndView("redirect:/app")
+}
