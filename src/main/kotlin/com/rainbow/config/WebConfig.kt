e7e99cf15d50c9b42dafdf25ce0b23a41a13281e
chencaihong <flq900523>
2017-06-28 22:30:42 +0800

    加入beetle配置

--- src/main/kotlin/com/rainbow/config/WebConfig.kt
+++ src/main/kotlin/com/rainbow/config/WebConfig.kt
@@ -5,13 +5,12 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 /**
- * Created by rainbow on 2017/6/28.
- *一事专注，便是动人；一生坚守，便是深邃！
+ * Created by Administrator on 2017/6/28.
  */
 @Configuration
 class WebConfig :WebMvcConfigurerAdapter(){
 
-    override fun addInterceptors(registry: InterceptorRegistry) {
+    override fun addInterceptors(registry: InterceptorRegistry?) {
         super.addInterceptors(registry)
     }
 }
